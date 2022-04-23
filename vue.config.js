0daa9c8a63edba5d4cd23f8484a5aeafa7c3eeb8
bahti <bahtirek@gmail.com>
2022-04-23 12:10:00 -0400

    compile config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     filenameHashing: false,
     productionSourceMap: false,
-    devServer: { host: 'localhost' }
+    devServer: { host: 'localhost' },
+    runtimeCompiler: true,
+    configureWebpack: {
+        //devtool: 'inline-source-map',
+        /* resolve: {
+            alias: {
+              'vue$': 'vue/dist/vue.runtime.js' // 'vue/dist/vue.common.js' for webpack 1
+            }
+          } */
+    }
 }
